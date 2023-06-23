b49d82acfdd5fca6c4c72d1c4baacabeae41fc64
ankitaagarwal123 <kgec.ankita@gmail.com>
2023-06-23 10:22:47 +0530

    video chat app try 2

--- server.js
+++ server.js
@@ -26,8 +26,8 @@ const transporter = nodemailer.createTransport({
     port: 465,
     host: "smtp.gmail.com",
     auth: {
-        user: '',
-        pass: '',
+        user: 'kgec.ankita@gmail.com',
+        pass: 'iezvshczorewomny',
     },
     secure: true,
 });
